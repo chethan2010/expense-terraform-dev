5b91070f36b34145b34f596f1893701382451755
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-05-29 20:39:01 +0530

    vpc

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -4,7 +4,7 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-resource "aws_ssm_parameter" "public_subnet_ids" {
+resource "aws_ssm_parameter" "public_subnet_id" {
   name  = "/${var.Project_name}/${var.environment}/public_subnet_ids"
   type  = "StringList"
   value = join("," ,module.vpc.public_subnet_ids) # converting list to string list
@@ -12,7 +12,7 @@ resource "aws_ssm_parameter" "public_subnet_ids" {
 
 #["id1","id2"] terraform format
 # id1, id2 -> AWS SSM format
-resource "aws_ssm_parameter" "private_subnet_ids" {
+resource "aws_ssm_parameter" "private_subnet_id" {
   name  = "/${var.Project_name}/${var.environment}/private_subnet_ids"
   type  = "StringList"
   value = join(",",module.vpc.private_subnet_ids) # converting list to string list
