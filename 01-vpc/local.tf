3c53cc4e35e48f56cdcde422befee2a723ef9927
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-05-29 16:44:55 +0530

    vpc

--- 01-vpc/local.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  resource_name = "${var.Project_name}-${var.Environment}"
-  az_names = slice(data.aws_availability_zones.available.names, 0,2)
-}
-
-
--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -1,115 +1,31 @@
-variable "Project_name" {
-    type = string
+variable "project_name" {
+    default = "expense"
 }
 
-variable "Environment" {
-    default = "dev"
+variable "environment" {
+  default = "dev"
 }
 
 variable "common_tags" {
-    type = map
-    
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
 }
-
-#Vpc Variables
-variable "cidr_block" {
-    default = "10.0.0.0/16"
-}
-
-
-variable "enable_dns_hostnames" {
-  type=bool  
-  default =true
-
-}
-variable "vpc_tags" {
-    default = {}
-  
-}
-
-#IGW
-variable "igw_tags" {
-    default = {}
-  
-}
-
-#Subnets
 
 variable "public_subnet_cidrs" {
-type = list
-validation{
-    condition = length(var.public_subnet_cidrs) ==2
-    error_message = "Please enter two Public_subnet_cidrs"
-}
+    default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-    type = list
-validation {
-  condition = length(var.private_subnet_cidrs) ==2
-  error_message = "Please enter two private subet cidrs"
-}
-  
+    default = ["10.0.11.0/24","10.0.12.0/24"]
 }
 
 variable "database_subnet_cidrs" {
-    validation {
-      condition = length(var.database_subnet_cidrs) ==2
-      error_message = "please enter two database subnet cidrs"
-    }
-  
-}
-
-
-variable "public_subnet_cidr_tags" {
-    default = {}
-  
-}
-
-variable "private_subnet_cidr_tags" {
-    default = {}
-  
-}
-
-variable "database_subnet_cidr_tags" {
-    default = {}
-  
-}
-
-variable "public_route_table_tags" {
-  default = {}
-}
-
-variable "nat_gateway_tags" {
-    default = {}
-  
-}
-
-
-#########Perring#
-variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-  
+    default = ["10.0.21.0/24","10.0.22.0/24"]
 }
 
 variable "is_peering_required" {
-        type = bool
-        default = false
-}
-
-variable "acceptor_vpc_id" {
-    type = string
-    default = ""
-  
-}
-
-
-variable "vpc_peering_tags" {
-    default = {}
-  
-}
-
-
-variable "aws_db_subnet_group_tags" {
-  default = {}
+  default = true
 }
