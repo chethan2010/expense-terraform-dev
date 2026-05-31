222154642b88e14bd6bcc2450da0aa56f723d082
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-05-31 15:54:48 +0530

    vpc

--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  backend "s3" {
     bucket         = "dev-daw9-hemanth-2026"
-    key            = "exp-dev-vpc/terraform.tfstate"
+    key            = "exp-dev-sg/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "daws83s"
   }
