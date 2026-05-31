aa6fb81d4e74cad15d740cdf26b8f2d8d9092655
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-05-31 15:40:08 +0530

    vpc

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -2,18 +2,21 @@ resource "aws_ssm_parameter" "vpc_id" {
   name  = "/${var.Project_name}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
   name  = "/${var.Project_name}/${var.environment}/public_subnet_ids"
   type  = "StringList"
   value = join(",", module.vpc.public_subnets_ids)
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "private_subnet_ids" {
   name  = "/${var.Project_name}/${var.environment}/private_subnet_ids"
   type  = "StringList"
   value = join(",", module.vpc.private_subnets_ids)
+  overwrite = true
 }
 
 
@@ -21,4 +24,5 @@ resource "aws_ssm_parameter" "db_subnet_group_name" {
   name  = "/${var.Project_name}/${var.environment}/db_subnet_group_name"
   type  = "String"
   value = module.vpc.database_subnet_group_name
+  overwrite = true
 }
